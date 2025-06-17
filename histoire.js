c2099af8daba720eb9c3175219b9bd1a90548ea6
Gabriel Arthus <gabriel.arthus@isen-ouest.yncrea.fr>
2025-06-17 18:05:18 +0200

    version qui marche mais a améliorer

--- histoire.js
+++ histoire.js
@@ -5,7 +5,7 @@ let health = 100;
 
 let enemyAttack = 50;
 let enemyDefense = 35;
-let enemyHealth = 250;
+let enemyHealth = 350;
 
 if (sessionStorage.getItem("attack")) {
   attack = parseInt(sessionStorage.getItem("attack"));
@@ -16,11 +16,6 @@ if (sessionStorage.getItem("defense")) {
 if (sessionStorage.getItem("strategy")) {
   strategy = parseFloat(sessionStorage.getItem("strategy"));
 }
-
-console.log("Initial Stats:");
-console.log("Attack:", attack);
-console.log("Defense:", defense);
-console.log("Strategy:", strategy);
 
 function updateStats() {
   attack = 0;
@@ -114,33 +109,175 @@ function enemyAttackFunction() {
 }
 
 function fight() {
-  // boucle de combat
-  if (sessionStorage.getItem("player-life") > 0 && sessionStorage.getItem("enemy-life") > 0) {
-    console.log("Ca, execute");
-    let fight_choose = document.querySelector('input[name="tour"]:checked').id;
-    if (fight_choose == "t1r1") {
-      enemy = enemyAttackFunction();
-      let attack_enemy = enemy.attack_enemy;
-      let defense_enemy = enemy.defense_enemy;
-      if (attack_enemy - defense > 0) {
-        health_fight = sessionStorage.getItem("player-life") - attack_enemy - defense;
-        console.log("Enemy attack:", attack_enemy - defense);
+  // Récupérer les valeurs actuelles de vie du joueur et de l'ennemi
+  let playerLife = parseInt(sessionStorage.getItem("player-life"));
+  let enemyLife = parseInt(sessionStorage.getItem("enemy-life"));
+
+  if (playerLife > 0 && enemyLife > 0) {
+    console.log("Début du combat");
+
+    // Vérifier le choix du joueur pour le tour
+    const fightChoose = document.querySelector('input[name="tour"]:checked').id;
+    if (fightChoose == "t1r1") {
+      const enemy = enemyAttackFunction();
+      const attackEnemy = enemy.attack_enemy;
+      const defenseEnemy = enemy.defense_enemy;
+
+      console.log("Enemy attack:", attackEnemy);
+      console.log("Enemy defense:", defenseEnemy);
+
+      // Calcul des dégâts infligés au joueur
+      if (attackEnemy - defense > 0) {
+        playerLife -= attackEnemy - defense;
+        console.log("Dégâts subis par le joueur :", attackEnemy - defense);
       }
-      if ((attack + 20) * strategy - defense_enemy > 0) {
-        enemyHealth_fight = sessionStorage.getItem("enemy-life") - (attack + 20) * strategy - defense_enemy;
-        console.log("Player attack:", (attack + 20) * strategy - defense_enemy);
+
+      // Calcul des dégâts infligés à l'ennemi
+      if ((attack + 20) * strategy - defenseEnemy > 0) {
+        enemyLife -= (attack + 20) * strategy - defenseEnemy;
+        console.log(
+          "Dégâts infligés à l'ennemi :",
+          (attack + 20) * strategy - defenseEnemy
+        );
       }
+      sessionStorage.setItem("player-life", Math.round(playerLife));
+      sessionStorage.setItem("enemy-life", Math.round(enemyLife));
+
+      console.log("Vie du joueur après le combat :", playerLife);
+      console.log("Vie de l'ennemi après le combat :", enemyLife);
+
+      // Vérifier les conditions de fin de combat
+      if (playerLife <= 0) {
+        console.log("Le joueur a perdu !");
+        sessionStorage.removeItem("player-life");
+        sessionStorage.removeItem("enemy-life");
+        window.location.href = "12.html"; // Page de défaite
+      } else if (playerLife < 30) {
+        console.log("Le joueur est gravement blessé !");
+        sessionStorage.removeItem("player-life");
+        sessionStorage.removeItem("enemy-life");
+        window.location.href = "13.html"; // Page de blessure critique
+      } else if (enemyLife <= 0) {
+        console.log("L'ennemi a été vaincu !");
+        sessionStorage.removeItem("player-life");
+        sessionStorage.removeItem("enemy-life");
+        window.location.href = "14.html"; // Page de victoire
+      } else {
+        console.log("Le combat continue !");
+        window.location.href = "11.html"; // Retour à la page de combat
+      }
+    } else if (fightChoose == "t1r2") {
+      console.log("Début du combat");
+      const enemy = enemyAttackFunction();
+      const attackEnemy = enemy.attack_enemy;
+      const defenseEnemy = enemy.defense_enemy;
+
+      console.log("Enemy attack:", attackEnemy);
+      console.log("Enemy defense:", defenseEnemy);
+
+      // Calcul des dégâts infligés au joueur
+      if (attackEnemy - (defense + 10) > 0) {
+        playerLife -= attackEnemy - (defense + 10);
+        console.log(
+          "Dégâts subis par le joueur :",
+          attackEnemy - (defense + 10)
+        );
+      }
+
+      // Calcul des dégâts infligés à l'ennemi
+      if ((attack - 10) * strategy - defenseEnemy > 0) {
+        enemyLife -= (attack - 10) * strategy - defenseEnemy;
+        console.log(
+          "Dégâts infligés à l'ennemi :",
+          (attack - 10) * strategy - defenseEnemy
+        );
+      }
+      sessionStorage.setItem("player-life", Math.round(playerLife));
+      sessionStorage.setItem("enemy-life", Math.round(enemyLife));
+
+      console.log("Vie du joueur après le combat :", playerLife);
+      console.log("Vie de l'ennemi après le combat :", enemyLife);
+
+      // Vérifier les conditions de fin de combat
+      if (playerLife <= 0) {
+        console.log("Le joueur a perdu !");
+        sessionStorage.removeItem("player-life");
+        sessionStorage.removeItem("enemy-life");
+        window.location.href = "12.html"; // Page de défaite
+      } else if (playerLife < 30) {
+        console.log("Le joueur est gravement blessé !");
+        sessionStorage.removeItem("player-life");
+        sessionStorage.removeItem("enemy-life");
+        window.location.href = "13.html"; // Page de blessure critique
+      } else if (enemyLife <= 0) {
+        console.log("L'ennemi a été vaincu !");
+        sessionStorage.removeItem("player-life");
+        sessionStorage.removeItem("enemy-life");
+        window.location.href = "14.html"; // Page de victoire
+      } else {
+        console.log("Le combat continue !");
+        window.location.href = "11.html"; // Retour à la page de combat
+      }
+    } else if (fightChoose == "t1r3") {
+      console.log("Début du combat");
+      const enemy = enemyAttackFunction();
+      const attackEnemy = enemy.attack_enemy;
+      const defenseEnemy = enemy.defense_enemy;
+
+      console.log("Enemy attack:", attackEnemy);
+      console.log("Enemy defense:", defenseEnemy);
+
+      // Calcul des dégâts infligés au joueur
+      if (attackEnemy - defense > 0) {
+        playerLife -= attackEnemy - defense;
+        console.log(
+          "Dégâts subis par le joueur :",
+          attackEnemy - defense
+        );
+      }
+
+      // Calcul des dégâts infligés à l'ennemi
+      if ((attack + 50) * strategy - defenseEnemy > 0) {
+        enemyLife -= (attack + 50) * strategy - defenseEnemy;
+        console.log(
+          "Dégâts infligés à l'ennemi :",
+          (attack + 50) * strategy - defenseEnemy
+        );
+      }
+      sessionStorage.setItem("player-life", Math.round(playerLife));
+      sessionStorage.setItem("enemy-life", Math.round(enemyLife));
+
+      console.log("Vie du joueur après le combat :", playerLife);
+      console.log("Vie de l'ennemi après le combat :", enemyLife);
+
+      // Vérifier les conditions de fin de combat
+      if (playerLife <= 0) {
+        console.log("Le joueur a perdu !");
+        sessionStorage.removeItem("player-life");
+        sessionStorage.removeItem("enemy-life");
+        window.location.href = "12.html"; // Page de défaite
+      } else if (playerLife < 30) {
+        console.log("Le joueur est gravement blessé !");
+        sessionStorage.removeItem("player-life");
+        sessionStorage.removeItem("enemy-life");
+        window.location.href = "13.html"; // Page de blessure critique
+      } else if (enemyLife <= 0) {
+        console.log("L'ennemi a été vaincu !");
+        sessionStorage.removeItem("player-life");
+        sessionStorage.removeItem("enemy-life");
+        window.location.href = "14.html"; // Page de victoire
+      } else {
+        console.log("Le combat continue !");
+        window.location.href = "11.html"; // Retour à la page de combat
+      }
+    } else {
+      console.error(
+        "Le combat ne peut pas commencer : vie du joueur ou de l'ennemi invalide."
+      );
+      sessionStorage.setItem("player-life", health);
+      sessionStorage.setItem("enemy-life", enemyHealth);
+      window.location.href = "11.html";
     }
-    sessionStorage.setItem("player-life", health_fight);
-    sessionStorage.setItem("enemy-life", enemyHealth);
-    window.location.href = "11.html";
-    return;
-  } else if (health <= 0) {
-    window.location.href = "12.html";
-  } else if (health < 30) {
-    window.location.href = "13.html";
-  } else {
-    window.location.href = "14.html";
   }
 }
 
@@ -180,11 +317,6 @@ document
       event.preventDefault();
       console.log("Fight() va commencer");
       fight();
-    } else {
-      console.log("Aucun choix de tour sélectionné");
-      event.preventDefault();
-      alert("Please select an action before proceeding.");
-      return;
     }
   });
 
