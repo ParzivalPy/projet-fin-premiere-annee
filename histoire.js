d883666b6efd50af19bbe84d11a46d5de4fa4420
Gabriel Arthus <gabriel.arthus@isen-ouest.yncrea.fr>
2025-06-17 10:12:33 +0200

    avancée dans la fonction fight

--- histoire.js
+++ histoire.js
@@ -3,6 +3,10 @@ let defense = 0;
 let strategy = 0;
 let health = 100;
 
+let enemyAttack = 50;
+let enemyDefense = 35;
+let enemyHealth = 250;
+
 if (sessionStorage.getItem("attack")) {
   attack = parseInt(sessionStorage.getItem("attack"));
 }
@@ -98,23 +102,16 @@ function updateStats() {
   sessionStorage.setItem("strategy", strategy);
 }
 
-function fight(attack, defense, strategy, health) {
-  let enemyAttack = 50;
-  let enemyDefense = 35;
-  let enemyHealth = 250;
-
+function fight(attack, defense, strategy, health, enemyAttack, enemyDefense, enemyHealth) {
   // boucle de combat
-  while (health > 0 && enemyHealth > 0) {
-    
-  }
-
-  //redirection en fonction de la santé
-  if (health <= 0) {
-    window.location.href = "14.html";
+  if (health > 0 && enemyHealth > 0) {
+    document.querySelector("input[name=\"tour\"]:checked");
+  } else if (health <= 0) {
+    window.location.href = "12.html";
   } else if (health < 30) {
-    window.location.href = "15.html";
+    window.location.href = "13.html";
   } else {
-    window.location.href = "16.html";
+    window.location.href = "14.html";
   }
 }
 
